24ee2a5a87c1a1b048316b4f41901db624416177
angelivvanparedes-ai <angelivvanparedes@gmail.com>
2026-02-12 19:19:20 -0600

    Agregada la funcion saludar

--- main.jl
+++ main.jl
@@ -5,3 +5,9 @@ end
 println("Hola mundo") 
 resultado = suma(2,3)
 println("Este es el resultado: $resultado")
+
+function saludo(nombre)
+    println("Hola, $nombre! Saludos para quien lo vea.")
+end
+
+saludo("Holaaaaa")
